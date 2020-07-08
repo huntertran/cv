06bee9e22fefae9f68e72614c2538ef11b719b64
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-07-08 16:50:33 -0400

    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = {
   transpileDependencies: [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === "production" ? "/cv/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/cv/" : "/",
   outputDir: "docs"
 }
